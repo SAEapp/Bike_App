7c41e865f8d7da339ff247b635f190851e69e546
Vibhanshu <VibhanshuV00@gmail.com>
2020-09-20 14:19:38 +0530

    rent estimation complete (almost)

--- app/src/main/java/com/example/bikeapp/BookingActivity.java
+++ app/src/main/java/com/example/bikeapp/BookingActivity.java
@@ -10,6 +10,7 @@ import android.text.InputFilter;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,7 +35,7 @@ public class BookingActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private FirebaseAuth fAuth;
     private FirebaseUser user;
-    public int est_cost;
+    public int est_cost,hourly_rent=40;
     private String userID;
     public String pickup_time,rent_duration,payment_mode;
     public Map<String, Object> info;
@@ -66,20 +67,35 @@ public class BookingActivity extends AppCompatActivity {
         hrs_dis.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "11")});
         min_dis.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "59")});
 
-        spinner = findViewById(R.id.spinner);
+        AdapterView.OnItemSelectedListener itemSelect = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String a = (String) parent.getItemAtPosition(position);
+                est_cost = rentDurationStringToInt(a)*hourly_rent;
+                est_price.setText(String.valueOf(est_cost));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+        spinner = findViewById(R.id.spinner);  //rent duration
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(BookingActivity.this,
                 R.layout.spinner_item_am, getResources().getStringArray(R.array.renting_hours));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(myAdapter);
+        spinner.setOnItemSelectedListener(itemSelect);
 //        spinner.getSelectedItem().toString();
 
-        spinner2 = findViewById(R.id.spinner2);
+        spinner2 = findViewById(R.id.spinner2);    //AM/PM
         ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(BookingActivity.this,
                 R.layout.spinner_item_am, getResources().getStringArray(R.array.am_pm));
         myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(myAdapter2);
 
-        spinner3 = findViewById(R.id.spinner3);
+        spinner3 = findViewById(R.id.spinner3);    //
         ArrayAdapter<String> myAdapter3 = new ArrayAdapter<String>(BookingActivity.this,
                 R.layout.spinner_item_am, getResources().getStringArray(R.array.payment_modes));
         myAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -101,7 +117,6 @@ public class BookingActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                loading.show();
                 rent_duration =  spinner.getSelectedItem().toString();
-                est_cost = 40;    //////////////////////////////////////////////////////incomplete
                 pickup_time = hrs_dis.getText().toString() + ":" + min_dis.getText().toString() + " " + spinner2.getSelectedItem().toString();
                 payment_mode = spinner3.getSelectedItem().toString();
                 Intent intent = new Intent(BookingActivity.this,PaymentActivity.class);
@@ -110,4 +125,23 @@ public class BookingActivity extends AppCompatActivity {
         });
 
     }
+
+    public int rentDurationStringToInt(String duration) {   //helper method
+       int time = 0;
+       if(duration.equals("1 hour")) {
+           time = 1;
+       } else if(duration.equals("2 hours")) {
+           time = 2;
+       } else if(duration.equals("3 hours")) {
+           time = 3;
+       } else if(duration.equals("4 hours")) {
+           time = 4;
+       } else if(duration.equals("5 hours")) {
+           time = 5;
+       } else if(duration.equals("6 hours")) {
+           time = 6;
+       }
+       return time;
+    }
+
 }
